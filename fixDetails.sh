8ea9ee897e3313934d8cebfef8d1e3d8b46fb98c
Grey Christoforo <grey@christoforo.net>
2016-03-01 17:23:47 +0000

    don't need some of these pacnew cleanups

--- fixDetails.sh
+++ fixDetails.sh
@@ -18,12 +18,13 @@ locale-gen
 # set timezone to UTC
 ln -s /usr/share/zoneinfo/UTC /etc/localtime
 
-# clean up the pacnews
+# clean up the pacnews by overwriting the origionals
 mv /etc/pacman.conf.pacnew /etc/pacman.conf || true
+mv /etc/hosts.pacnew /etc/hosts || true
+mv /etc/resolv.conf.pacnew /etc/resolv.conf || true
 #mv /etc/pacman.d/mirrorlist.pacnew /etc/pacman.d/mirrorlist || true
-mv /etc/shadow.pacnew /etc/shadow || true
-mv /etc/passwd.pacnew /etc/passwd || true
-#mv /etc/resolv.conf.pacnew /etc/resolv.conf || true
+#mv /etc/shadow.pacnew /etc/shadow || true
+#mv /etc/passwd.pacnew /etc/passwd || true
 
 # fix TERM not being set
 echo "export TERM=xterm" >> /etc/profile
